a86e94ca5c25049cfae7b08d1762ea42ddebc338
astorlucas <lucasastor@gmail.com>
2025-07-07 22:09:08 -0300

    filtering inline

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
   public static void main(String[] args) {
 
 
-    var pets = List.of("dog", "cat", "bird");
+    var pets = List.of("Dog", "Cat", "Bird");
 
     //pets.forEach( (p) -> { System.out.println(p); } );
 
@@ -16,6 +18,10 @@ public class Main {
       System.out.println("Pet number:" + pets.indexOf(x));
     });
 
+
+    Predicate<String> filterPets = x -> x.startsWith("D");
+
+    pets.stream().filter(t -> filterPets.test(t)).forEach(System.out::println);
 
     // ArrayList<Integer> numbers = new ArrayList<Integer>();
     // numbers.add(5);
